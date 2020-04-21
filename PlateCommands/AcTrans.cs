4c20f80ecf89874a99f9d46bc58c004ffb764e13
Sampaul <38540262+sampaulp@users.noreply.github.com>
2020-04-22 01:26:21 +0200

    Add files via upload

---
+++ PlateCommands/AcTrans.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace Tools
+{
+    // Database interface
+    class AcTrans
+    {
+        // write the AC entity into the AC database
+        public static void Add(Entity entity)
+        {
+            // get database reference
+            Database db = Application.DocumentManager.MdiActiveDocument.Database;
+
+            using (Transaction acTrans = db.TransactionManager.StartTransaction())
+            {
+                Log.Append("> Start Transaction...");
+
+                try
+                {
+                    // create a block table record
+                    BlockTableRecord acBTR = (BlockTableRecord)acTrans.GetObject(
+                                db.CurrentSpaceId,      // space id (f.e. model space)
+                                OpenMode.ForWrite);
+
+                    // put entity into block table record
+                    acBTR.AppendEntity(entity);
+
+                    // put it into the transaction
+                    //                                      |true add, false delete
+                    acTrans.AddNewlyCreatedDBObject(entity, true);
+
+                    // write data into the database completing the transaction
+                    acTrans.Commit();
+
+                    Log.Append("> Transaction successfull!");
+                }
+
+                // handle autocad exception
+                catch(Autodesk.AutoCAD.Runtime.Exception ex)
+                {
+                    acTrans.Abort();
+                    Log.Append("*** error in transaction!");
+                    Log.Append(ex.ToString());
+                }
+
+                // clearing
+                finally
+                {
+                    GC.Collect();   // clear the memory
+                }
+                Log.Append("> completed!");
+            }
+
+        }
+
+    }
+}
---
+++ PlateCommands/Log.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.ApplicationServices;
+
+namespace Tools
+{
+    // logger class should trace information
+    // into different devices
+    // - output into the console window
+    // - output into a log file
+    class Log
+    {
+        // log file name
+        private static string _sFileName = "Autocad.log";
+
+        // setter/getter function
+        public static string sFileName
+        {
+            get { return _sFileName;  }     // ... = Log.sFileName
+            set { _sFileName = value; }     // Log.sFileName = ...
+        }
+
+        // reset the log information
+        public static void Reset()
+        {
+            File.Delete(_sFileName);
+        }
+
+        // append a message to logging information
+        public static void Append(string msg)
+        {
+            // open for append and write log into log file
+            StreamWriter f = new StreamWriter(_sFileName, true);
+            f.WriteLine(DateTime.Now + "| " + msg);
+            f.Close();
+
+            // write msg into AutoCAD command line
+            Editor edit = Application.DocumentManager.MdiActiveDocument.Editor;
+            edit.WriteMessage(msg + "\n");
+        }
+
+    }
+}
